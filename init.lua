dd4a1b33d24208ae051ecd7fb8399dc65633a8ac
Jim Myers <jmyers@brex.com>
2024-02-27 17:38:12 +0000

    feat: enable autocompletion with nvim-cmp

--- init.lua
+++ init.lua
@@ -58,10 +58,21 @@ require('lazy').setup({
           "tflint",
           "yamlls",
         },
+        handlers = {
+        },
       })
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       require('mason-lspconfig').setup_handlers {
         function(server_name)
-          require('lspconfig')[server_name].setup {}
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+          }
+        end,
+        ["lua_ls"] = function()
+          require('lspconfig').lua_ls.setup {
+            settings = { Lua = { diagnostics = { globals = { 'vim' } } } },
+            capabilities = capabilities,
+          }
         end,
       }
     end,
@@ -104,6 +115,54 @@ require('lazy').setup({
   { 'windwp/nvim-autopairs', config = true },
   { 'nvim-lua/plenary.nvim' },
   { 'github/copilot.vim' },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "tmux" },
+        }),
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end
+  },
 })
 
 local opts = { noremap = true, silent = true }
@@ -147,10 +206,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
   end,
 })
-
-require('lspconfig')['lua_ls'].setup {
-  settings = { Lua = { diagnostics = { globals = { 'vim' } } } },
-}
 
 require('fzf-lua').setup {
   keymap = {
