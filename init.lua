c538f2a9d687c527a11f3cd78af1afa30b24ed4f
Jim Myers <jmyers@brex.com>
2024-02-27 17:40:52 +0000

    chore: remove errant handlers

--- init.lua
+++ init.lua
@@ -58,8 +58,6 @@ require('lazy').setup({
           "tflint",
           "yamlls",
         },
-        handlers = {
-        },
       })
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       require('mason-lspconfig').setup_handlers {
